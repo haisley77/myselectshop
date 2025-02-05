f8e90c9c7f2f150ad10a708ef561e83cea37cb21
haisley77 <hahi1108@gmail.com>
2025-02-06 00:13:45 +0900

    feat: myselectshop project

---
+++ src/main/java/com/sparta/myselectshop/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.sparta.myselectshop.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // 아래 설정을 등록하여 활성화 합니다.
+//@EnableJpaAuditing // 시간 자동 변경이 가능하도록 합니다.
+public class JpaConfig {
+}
---
+++ src/main/java/com/sparta/myselectshop/exception/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.sparta.myselectshop.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// 모든 controller 에서 발생한 예외에 대한 처리를 담당하는 handler
+// 예외 처리 중앙 집중화 -> 코드 중복 줄이고, 유지보수성 up, 개발 생산성 향상
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<RestApiException> illegalArgumentExceptionHandler(IllegalArgumentException ex) {
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(
+                // HTTP body
+                restApiException,
+                // HTTP status code
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler({NullPointerException.class})
+    public ResponseEntity<RestApiException> nullPointerExceptionHandler(NullPointerException ex) {
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<>(
+                // HTTP body
+                restApiException,
+                // HTTP status code
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler({ProductNotFoundException.class})
+    public ResponseEntity<RestApiException> notFoundProductExceptionHandler(ProductNotFoundException ex) {
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<>(
+                // HTTP body
+                restApiException,
+                // HTTP status code
+                HttpStatus.NOT_FOUND
+        );
+    }
+}
